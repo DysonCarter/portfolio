6521e55ff6dc0e8430344173d802b155ec3c5608
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2026-01-22 16:03:43 -0500

    Add html for about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,58 @@
-function About(){
-    return<>About</>
+function About() {
+  return(
+    <>
+      <h1>About Me</h1>
+
+      <p>
+        I'm a Computer Science student at the University of Akron who enjoys
+        building React apps and interested in game theory and
+        algorithmic problem-solving. I currently work with DataAnnotation, where
+        I help train, evaluate, and improve AI models by producing high-quality,
+        structured feedback.
+      </p>
+
+      <h2>Accomplishments</h2>
+      <ul>
+        <li>
+          Computer Science, B.S. — University of Akron (Expected May 2026)
+        </li>
+        <li>
+          Artificial Intelligence and Machine Learning Certificate — University of Akron
+        </li>
+      </ul>
+
+      <h2>Work History</h2>
+
+      <div>
+        <h3>University of Akron - Computer-Based Assessment Proctor</h3>
+        <p>August 2022 - December 2023</p>
+        <p>
+          Monitored students during exams at a testing center, ensured academic
+          integrity, and resolved technical issues in real time.
+        </p>
+      </div>
+
+      <div>
+        <h3>Sam's Club — Front End Associate</h3>
+        <p>December 2023 - June 2024</p>
+        <p>
+          Worked as a cashier and exit door associate, identifying and preventing
+          theft by verifying receipts and spotting inconsistencies - I had weeks of
+          catching over $200 worth of unpaid merchandise per shift!
+        </p>
+      </div>
+
+      <div>
+        <h3>DataAnnotation — AI Trainer</h3>
+        <p>May 2024 - Present</p>
+        <p>
+          Train and evaluate large language models by completing programming-heavy tasks, 
+          writing and reviewing prompts, and refining the model responses to improve 
+          accuracy, reasoning, structured output, and instruction-following.
+        </p>
+      </div>
+    </>
+  );
 }
+
 export default About;
