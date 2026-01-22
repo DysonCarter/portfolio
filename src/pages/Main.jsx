9a50989d8d97c95ec44440a00126198f41faeddc
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2026-01-22 15:05:47 -0500

    Add header

---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+function Main(){
+    return <>
+    <header>
+      <h1>Dyson Carter's Site</h1>
+      <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="mailto:dysonac04@gmail.com">Email</a>
+      </nav>
+    </header>
+    </>
+}
+
+export default Main;
